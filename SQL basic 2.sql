27df67dabcc91283efa40a1f7b53f487883e55d0
jaideepd786 <109033278+jaideepd786@users.noreply.github.com>
2024-01-19 12:49:33 +0530

    SQL basic 2.sql

--- SQL basic 2.sql
+++ SQL basic 2.sql
@@ -1,92 +1,79 @@
 show databases;
-use classicmodels;
-show tables;
-select * from customers;
-select contactFirstName,contactLastName,creditLimit from customers;
-select  * from employees;
-select firstName,lastName,jobTitle from employees;
-select contactFirstName,contactLastName,creditLimit 
-from customers where creditLimit>50000 and creditLimit<100000;
+use classicmodels;       -- use sample databse classicmodels
 
-select contactFirstName,contactLastName,creditLimit from customers where creditLimit<50000;
+show tables;             -- show tables of classicmodes DB
+
+select * from customers;   -- display customers tables
+
+select contactFirstName,contactLastName,creditLimit from customers;   -- display specific columns from customers table
+
+select * from employees;        -- display employee table
+ 
+select firstName,lastName,jobTitle from employees;                   -- display specific columns from employee table
+
+select contactFirstName,contactLastName,creditLimit from customers where creditLimit>50000 and creditLimit<100000;   -- display specific columns from customers table based on particular condition
+
+select contactFirstName,contactLastName,creditLimit from customers where creditLimit<50000;            
+
 select country from customers;
+
 select contactFirstName,contactLastName,creditLimit,country from customers where country='USA';
+
 select contactFirstName,contactLastName,creditLimit,country from customers where creditLimit>100000 and country='USA';
-select distinct(country) from customers;
-select count(distinct(country)) from customers;
-select country,count(customerNumber) from customers group by country;
 
-select count(*) from customers where state is null;
-select count(*) from customers where phone is null;
-select count(*) from customers where city is null;
-select count(*) from customers where creditLimit is null;
-select count(*) from customers where postalCode is null;
-select * from customers where state is null;
+select distinct(country) from customers;                       -- display unique values of column from customer table
 
-select contactFirstName,contactLastName,state,country 
-from customers 
-where state is not null;
+select count(distinct(country)) from customers;                -- display number of unique values 
 
-select productName,productLine,MSRP 
+
+show tables;
+
+select * from employees;
+
+select * from offices;
+
+select * from orders;
+
+select * from products;
+
+select * from payments;
+
+select * from customers;
+
+select count(productLine) as count_Motcycle from products where productLine='Motorcycles';     -- Aggreagate count function based on condition
+
+select sum(MSRP) as Total_Motcycle from products where productLine='Motorcycles';              -- Aggreagate SUM function based on condition
+
+select avg(MSRP) as avg_Motcycle from products where productLine='Motorcycles';                -- Aggreagate AVG function based on condition
+
+select min(MSRP) as min_Motcycle from products where productLine='Motorcycles';                -- Aggreagate MIN function based on condition
+
+select max(MSRP) as max_Motcycle from products where productLine='Motorcycles';                -- Aggreagate MAX function based on condition
+
+select productLine,count(productLine) as count_PL                        -- Group by clause to group unique value and use agregate funciton
 from products
-order by MSRP desc limit 5;
+group by productLine;
 
-select contactFirstName,contactLastName,country
-from customers
-where country in ('USA','UK','Spain');
-
-select productName from products;
-
-select distinct(productName) from products;
-
-select contactFirstName,contactLastName,creditLimit 
-from customers
-where creditLimit between 50000 and 100000
-order by creditlimit desc;
-
-select min(MSRP), max(MSRP) from products;
-
-select productName,productLine,MSRP
-from products
-where MSRP between 50 and 150;
-
-select contactFirstName,contactLastName,creditLimit 
-from customers
-where contactLastName like '%on%';
-
-select contactFirstName,contactLastName,creditLimit 
-from customers
-where contactFirstName like 'p%r';
-
-select count(productline) 
-from products 
-where productline='Classic Cars';
-
-select sum(MSRP) as total
-from products
-where productLine='Classic Cars';
-
-select productLine from products;
-
-select sum(MSRP) as total_MSRP,productLine
-from products
+select productLine,count(productLine) as count_PL,min(MSRP) as count_MIN,max(MSRP) as count_MAX,
+sum(MSRP) as count_SUM,avg(MSRP) as count_AVG
+from products                                                            -- group by clause with aggregate function 
 group by productLine
-order by total_MSRP desc;
+order by count_PL desc limit 5;                                          -- order by function to order the result based on column (limit to display n records)
 
-select count(MSRP) as cc_count, sum(MSRP) as cc_total, avg(MSRP) as cc_avg, min(MSRP) as cc_min, max(MSRP) as cc_max
-from products;
+select country, count(creditlimit) as CC_count, min(creditLimit),max(creditLimit),avg(creditLimit)
+from customers
+group by country                                                       
+having CC_count>10                                                   -- having clause(can be used only with groupby clause) to proide condition
+order by CC_count desc;
 
-select count(customerNumber) 
-from customers 
-where country='USA';
+select jobTitle, count(jobTitle) as jobtitle_count
+from employees
+group by jobtitle
+order by jobtitle_count desc;
 
-select min(creditLimit) 
-from customers 
-where country='UK';
-
-select avg(creditLimit) 
-from customers 
-where country='Spain';
+select creditlimit
+from customers
+order by creditlimit desc limit 1;
 
 
 
